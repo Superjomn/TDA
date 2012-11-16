e32f067ffbd910262f79dbc1aca560f1e811d849
superjom <superjom@gmail.com>
2012-11-17 02:29:55 +0800

    add textnode, not tested

--- pySST/sst/sourceparser.py
+++ pySST/sst/sourceparser.py
@@ -3,14 +3,16 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 from pyquery import PyQuery as pq
-from styletree import StyleTree, ElementNode, StyleNode
+from styletree import StyleTree, ElementNode, StyleNode, DataNode
 
 class Stack:
     def __init__(self):
         self.datas = []
     def push(self, data):
+        print '.. push', data
         self.datas.append(data)
     def pop(self):
+        print '.. pop'
         return self.datas.pop()
     def size(self):
         return len(self.datas)
@@ -19,6 +21,9 @@ class Stack:
     def empty(self):
         return self.size() == 0
 
+nodenames = ['a', 'p', 'b',]
+from copy import deepcopy as dc
+from styletree import getTagName
 class SourceParser:
     '''
     parse html source and add nodes to styletree
@@ -42,17 +47,38 @@ class SourceParser:
         self.parseIter()
 
     def parseIter(self):
-        print 'parseIter'
-        if not self.stack.empty():
-            (node , element) = self.stack.pop()
+        def addDataNode(fnode):
+            children = fnode.children()
+            for i in range(len(children)):
+                child = children.eq(i)
+                if getTagName(child) in nodenames:
+                    dn = DataNode()
+                    dn.setTagNode(child)
+                    fnode.registerStyleNode(dn)
+            #add text node
+            dn = DataNode()
+            if dn.setTextNode(node):
+                fnode.registerStyleNode(dn)
+
+        def addStyleNode(node):
+            _node = dc(node)
+            #clean node
+            childnodes = _node.children()
+            nodes = ['a', 'p', 'b',]
+            for n in nodes:
+                _node.remove(n)
             stylenode = StyleNode()
-            childnodes = node.children()
+            stylenode.generateStyleNode(_node)
+            _stylenode = element.registerStyleNode(stylenode)
             for i in range(len(childnodes)):
-                childnode = childnodes.eq(i)
-                html = childnodes
-                element = ElementNode(self._getTag(childnode))
-                stylenode.addChildElement(element)
-            element.registerStyleNode(stylenode)
+                childnode = _stylenode.getChild(i)
+                self.stack.push([ childnodes.eq(i), childnode ])
+                
+        while not self.stack.empty():
+            (node , element) = self.stack.pop()
+            #print '.. stylenode: ', _stylenode
+            addStyleNode(node)
+            addDataNode(node)
 
     def _getTag(self, node):
         end = str(node).index('>')
@@ -60,9 +86,20 @@ class SourceParser:
 
 
 if __name__ == '__main__':
+    strr = """
+    <html>
+        <body>
+            <div id='head1'>hello
+                <b id = 'hell'> b1</b>
+                <b id = 'hell'> b1</b>
+            </div>
+        </body>
+    </html>
+    """
+    #print 'content', len(content)
+    #strr = open('html').read()
     sourceparser = SourceParser()
-    content = open('html').read()
-    print 'content', len(content)
-    sourceparser.setSource(content)
+    sourceparser.setSource(strr)
     sourceparser.parse()
+    sourceparser.styletree.show()
     
--- pySST/sst/styletree.py
+++ pySST/sst/styletree.py
@@ -18,6 +18,17 @@ Created on Nov 11, 2012
 # *#################################################*/
 #from type import Node
 import math
+def getTag(node):
+    end = str(node).index('>')
+    return str(node)[:end+1]
+
+import re
+def getTagName(node):
+    t = re.compile("^<[\s]*(\S*)[\s>]")
+    res = t.findall(str(node))[0]
+    if res:
+        return res[0]
+    return False
 
 class ElementNode:
     '''
@@ -37,6 +48,9 @@ class ElementNode:
         self._imp = 0
         self.type = 'elementnode'
 
+    def setName(self, name):
+        self._name =  name
+
     def getName(self):
         return self._name
 
@@ -48,6 +62,26 @@ class ElementNode:
 
     def setImp(self, imp):
         self._imp = imp
+
+    def addDataNode(self, node):
+        '''
+        add text img a p b 
+        '''
+        nodenames = [
+            'img',
+            'IMG',
+            'P',
+            'p',
+            'b',
+            'B',
+        ]
+        datanode = DataNode()
+        tagname = getTagName(node)
+        if tagname in nodenames:
+            datanode.setTagNode(node)
+            self.addChildStyleNode(datanode)
+        elif datanode.setTextNode(node):
+            self.addChildStyleNode(datanode)
         
     def addChildStyleNode(self, node):
         '''
@@ -74,19 +108,22 @@ class ElementNode:
         node = self._searchStyleNode(stylenode.getPreview())
         if node:
             node.incCount()
+            print '.. return node ', node
             return node
         else:
             self.addChildStyleNode(stylenode)
+            print '.. return stylenode ', stylenode
+            return stylenode
 
     def __str__(self):
-        res = '(tagname: ' + str(self.getName()) + '\n'
-        res += '['
-        for child in self.getChildStyleNodes():
-            res += str(child)
-        res += '])'
+        #print 'element str:'
+        res = "[.{%s} " % self.getName()
+        for node in self.getChildStyleNodes():
+            res += str(node)
+            res += " "
+        res += "]"
         return res
         
-
 class StyleNode:
     def __init__(self):
         self._preview = ''
@@ -94,6 +131,15 @@ class StyleNode:
         self._count = 1
         self._children = []
         self.type = 'stylenode'
+
+    def generateStyleNode(self, node):
+        childnodes = node.children()
+        for i in range(len(childnodes)):
+            childnode = childnodes.eq(i)
+            element = ElementNode(self._getTag(childnode))
+            print '.. Element : ',element
+            self.addChildElement(element)
+
 
     def getPreview(self):
         #return self._preview
@@ -110,6 +156,9 @@ class StyleNode:
 
     def addChildElement(self, childElement):
         self._children.append(childElement)
+
+    def getChild(self, i):
+        return self._children[i]
 
     def generatePreview(self):
         res = ''
@@ -131,17 +180,49 @@ class StyleNode:
         self._imp  = imp
 
     def __str__(self):
-        res = 'stylenode:'
-        res += self.getPreview()
-        res += '['
-        for child in self.getChildrenElements():
-            res += str(child)
+        #print 'stylenode str:'
+        res = '[.' + "{(%s)}"%self.getPreview()
+        res += " "
+        for element in self.getChildrenElements():
+            res += str(element)
         res += ']'
         return res
 
+    def _getTag(self, node):
+        end = str(node).index('>')
+        return str(node)[:end+1]
+
+from copy import deepcopy as dc
+class DataNode(ElementNode):
+    '''
+    for nodes like b p img a
+    '''
+    def __init__(self, data = ''):
+        ElementNode.__init__(self, hash(data))
+
+    def setTextNode(self, node):
+        '''
+        text node and save hash
+        '''
+        _node = dc(node)
+        _nodechildren = _node.children()
+        for i in range(len(_nodechildren)):
+            _node.remove(_nodechildren.eq(i))
+        res = str(_node)
+        if res.lstrip():
+            self.setName (hash(str(_node)))
+            return True
+        return False
+
+    def setTagNode(self, node):
+        '''
+        img a p b
+        '''
+        self.setName(hash(str(node)))
+
 class StyleTree:
     def __init__(self):
-        self.body = ElementNode()
+        self.body = ElementNode('body')
         #num of sitepages
         self.pageNum = 0
 
@@ -182,12 +263,26 @@ class StyleTree:
         node.setImp(res)
         return res
 
+    def show(self):
+        '''
+        show structure
+        '''
+        print self.body
+
+
 if __name__ == '__main__':
-    ele = ElementNode('body')
+    body = ElementNode('body')
+    stylenode = StyleNode()
     p = ElementNode('p')
     b = ElementNode('b')
-    stylenode = StyleNode()
     stylenode.addChildElement(p)
     stylenode.addChildElement(b)
-    ele.registerStyleNode(stylenode)
-    print ele
+    print stylenode
+    body.registerStyleNode(stylenode)
+    q = ElementNode('q')
+    c = ElementNode('c')
+    stylenode2 = StyleNode()
+    stylenode2.addChildElement(q)
+    stylenode2.addChildElement(c)
+    b.registerStyleNode(stylenode2)
+    print 'body', body
